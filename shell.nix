3360885669912b92cbd909dcac03edfe96350dc1
juno-soyr <h.gaspar@proton.me>
2025-10-18 13:08:08 +0200

    Push

--- shell.nix
+++ shell.nix
@@ -1,19 +1,23 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
-  pkgs = import nixpkgs {
-    config = { };
-    overlays = [ ];
-  };
-in
-pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    jogl
-    gradle
-    jdk
-    metals
-    libGL
-    xorg.libXxf86vm
-    maven
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
-  ];
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              (scala.override { jre = pkgs.jdk8; })
+              metals
+              gradle_6
+            ];
+          };
+        };
+    };
 }
