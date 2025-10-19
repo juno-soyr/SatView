3ebe69571731249dd0a767f488700960f5fa888c
juno-soyr <h.gaspar@proton.me>
2025-10-19 04:18:13 +0200

    python lsp

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 To get started, run the following:
 
+
 ```
 $ nix develop
 $ poetry run python -m sample_package
--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
+import numpy
+import pandas
 import sgp4
+
 if __name__ == "__main__":
+
+
+aaaa 
+
+
+
     print("Start")
